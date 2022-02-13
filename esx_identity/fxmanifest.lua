e6a1ce7ba48020df91fe473d357c659ee873d84e
Gustavo Alfredo Soria <93205175+tavostro@users.noreply.github.com>
2022-02-13 16:58:16 -0300

    Add files via upload

---
+++ esx_identity/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Identity'
+
+version '1.4.0'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/cs.lua',
+	'locales/pl.lua',
+	'locales/fi.lua',	
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/cs.lua',
+	'locales/pl.lua',
+	'locales/fi.lua',	
+	'config.lua',
+	'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/js/script.js',
+	'html/css/style.css',
+	'html/img/fondo.jpg'
+	'html/img/logo.png'
+}
+
+dependency 'es_extended'
